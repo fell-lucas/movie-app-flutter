f537f8c109efbb6d265efbca013f2c1fdb83deac
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-11-10 11:09:33 -0300

    movie bloc

--- lib/home/blocs/movie/movie_bloc.dart
+++ lib/home/blocs/movie/movie_bloc.dart
@@ -1,13 +1,35 @@
+import 'dart:html';
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:movie_repository/movie_repository.dart';
 
 part 'movie_event.dart';
 part 'movie_state.dart';
 
 class MovieBloc extends Bloc<MovieEvent, MovieState> {
-  MovieBloc() : super(MovieInitial()) {
-    on<MovieEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final MovieRepository movieRepository;
+
+  MovieBloc({
+    required this.movieRepository,
+  }) : super(MovieInitial());
+
+  @override
+  Stream<MovieState> mapEventToState(MovieEvent event) async* {
+    yield MovieLoadInProgress();
+    if (event is SearchMovies) {
+      yield* _mapSearchMoviesToState(event);
+    }
+  }
+
+  Stream<MovieState> _mapSearchMoviesToState(SearchMovies event) async* {
+    try {
+      List<Movie> movies = await movieRepository.searchMovies(fts: event.fts);
+      yield MovieSearchLoadSuccessful(movies: movies);
+    } on HttpException catch (e) {
+      yield MovieError(error: e.message);
+    }
   }
 }
--- lib/home/blocs/movie/movie_event.dart
+++ lib/home/blocs/movie/movie_event.dart
@@ -1,4 +1,17 @@
 part of 'movie_bloc.dart';
 
 @immutable
-abstract class MovieEvent {}
+abstract class MovieEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class SearchMovies extends MovieEvent {
+  final String fts;
+  SearchMovies({
+    required this.fts,
+  });
+
+  @override
+  List<Object?> get props => [fts];
+}
